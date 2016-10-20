54c46322ae7dfb735562d1d9f38aba3bf5f7d5da
Wesley Turner-Harris <wturnerharris@gmail.com>
2016-10-20 16:24:01 -0400

    Change recipie to deploy rather than configure

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -1,5 +1,5 @@
 # AWS OpsWorks Recipe for Wordpress to be executed during the Configure lifecycle phase
-# - Creates the config file wp-config.php with MySQL data.
+# - Creates the config file wp-config-local.php with MySQL data.
 # - Creates a Cronjob.
 # - Imports a database backup if it exists.
 
