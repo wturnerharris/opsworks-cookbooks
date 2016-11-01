ceab955ac930c7859ef21cdf8582e408246324da
Wesley Turner-Harris <wturnerharris@gmail.com>
2016-11-01 15:11:37 -0400

    Update chef script to import db if found

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -14,7 +14,7 @@ http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 request = Net::HTTP::Get.new(uri.request_uri)
 response = http.request(request)
 keys = response.body
-
+home = "/home/ec2-user"
 
 # Create the Wordpress config file wp-config.php with corresponding values
 node[:deploy].each do |app_name, deploy|
@@ -52,21 +52,20 @@ node[:deploy].each do |app_name, deploy|
         end
     end
     
-    Chef::Log.debug("Importing .htaccess and files from backup...")
+    Chef::Log.debug("Importing files from backup...")
     script 'deploy_files' do
       interpreter "bash"
       user "root"
-      cwd "/home/ec2-user"
+      cwd "#{home}/"
       code <<-EOH
-        HOME=/home/ec2-user
-        FILE=$(ls -t entre* | head -1)
-        ROOT=/srv/www/era/current
+        HOME=#{home}
+        FILE=$(ls -t entre*files.tar.gz | head -1)
+        ROOT=#{deploy[:deploy_to]}/current
 
         cd $HOME
         pwd
         tar -xvzf $FILE
         ls -la
-        cp $HOME/htaccess $ROOT/.htaccess
         rm -rf $ROOT/wp-content/uploads
         mv $HOME/files_develop $ROOT/wp-content/uploads
         ls -la
@@ -78,16 +77,20 @@ node[:deploy].each do |app_name, deploy|
     # Import Wordpress database backup from file if it exists
     mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' :  "-p#{node[:mysql][:server_root_password]}"} #{deploy[:database][:database]}"
 
-    Chef::Log.debug("Importing Wordpress database backup...")
-    script "memory_swap" do
+    Chef::Log.debug("Importing Wordpress database from backup...")
+    script "import_sql" do
       interpreter "bash"
       user "root"
-      cwd "#{deploy[:deploy_to]}/current/"
+      cwd "#{home}/"
       code <<-EOH
-        if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then 
-        #{mysql_command} < #{deploy[:deploy_to]}/current/*.sql;
-        rm #{deploy[:deploy_to]}/current/*.sql;
+        HOME=#{home}
+        FILE=$(ls -t entre*database.sql.gz | head -1)
+
+        if [ -f $FILE ]; then 
+          #{mysql_command} < $FILE;
+          rm $FILE;
         fi;
+
         EOH
     end
 
