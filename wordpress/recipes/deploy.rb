b039511ea7aaec00c553df6a5ec9dafda7f52d9a
Wesley Turner-Harris <wturnerharris@gmail.com>
2016-11-03 17:27:00 -0400

    Refactor deploy recipe
    * Move import-specific scripts to import recipe
    * Remove import-specific scripts from deploy recipie
    
    TODO: Add ENV_VARS to allow wp search-replace to be more dynamic.

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -14,7 +14,6 @@ http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 request = Net::HTTP::Get.new(uri.request_uri)
 response = http.request(request)
 keys = response.body
-home = "/home/ec2-user"
 
 # Create the Wordpress config file wp-config.php with corresponding values
 node[:deploy].each do |app_name, deploy|
@@ -52,63 +51,6 @@ node[:deploy].each do |app_name, deploy|
         end
     end
     
-    Chef::Log.info("Importing files from backup...")
-    script 'deploy_files' do
-      interpreter "bash"
-      user "root"
-      cwd "#{home}/"
-      code <<-EOH
-        HOME=#{home}
-        FILE=$(ls -t entre*files.tar.gz | head -1)
-        ROOT=#{deploy[:deploy_to]}/current
-
-        cd $HOME
-        pwd
-        tar -xvzf $FILE
-        ls -la
-        rm -rf $ROOT/wp-content/uploads
-        mv $HOME/files_develop $ROOT/wp-content/uploads
-        ls -la
-        
-        EOH
-      not_if { ::File.exists?("#{home}/files_develop") }
-    end
-
-    # Import Wordpress database backup from file if it exists
-    mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' :  "-p#{node[:mysql][:server_root_password]}"} #{deploy[:database][:database]}"
-
-    Chef::Log.info("Importing Wordpress database from backup...")
-    script "import_sql" do
-      interpreter "bash"
-      user "root"
-      cwd "#{home}/"
-      code <<-EOH
-        HOME=#{home}
-        FILE=$(ls -t entre*database.sql.gz | head -1)
-        ROOT=#{deploy[:deploy_to]}/current
-        WPCLI=`wp --info`
-
-        if [ -f $FILE ]; then 
-          #{mysql_command} < $FILE;
-        fi;
-        
-        if ! type "$WPCLI" &>/dev/null; then 
-          cd $HOME
-          curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-          php wp-cli.phar --info
-          chmod +x wp-cli.phar
-          sudo mv wp-cli.phar /usr/local/bin/wp
-        else
-          cd $ROOT
-          wp search-replace 'develop-entrepreneurs-roundtable-accelerator.pantheonsite.io' 'test.eranyc.com' --allow-root
-          wp search-replace 'ec2-52-15-34-117.us-east-2.compute.amazonaws.com' 'test.eranyc.com' --allow-root
-          wp search-replace 'era.dev' 'test.eranyc.com' --allow-root
-          wp search-replace 'http://test.eranyc.com' 'https://test.eranyc.com' --allow-root
-        fi
-
-        EOH
-    end
-
 end
 
 # Create a Cronjob for Wordpress
--- wordpress/recipes/import.rb
+++ wordpress/recipes/import.rb
@@ -1,28 +1,68 @@
-# AWS OpsWorks Recipe for Wordpress to be executed during the Configure lifecycle phase
-# - Creates the config file wp-config-local.php with MySQL data.
-# - Creates a Cronjob.
-# - Imports a database backup if it exists.
+# AWS OpsWorks Recipe to import WordPress files and database
+# - Deploys 
+# - Imports compressed tar files archive.
+# - Imports compressed MySQL archive.
 
-require 'aws-sdk'
+home = "/home/ec2-user"
 
-s3 = AWS::S3.new
-s3_bucket = node[:wordpress][:s3]
+# Create the Wordpress config file wp-config.php with corresponding values
+node[:deploy].each do |app_name, deploy|
+    
+    Chef::Log.info("Importing files from backup...")
+    script 'deploy_files' do
+      interpreter "bash"
+      user "root"
+      cwd "#{home}/"
+      code <<-EOH
+        HOME=#{home}
+        FILE=$(ls -t entre*files.tar.gz | head -1)
+        ROOT=#{deploy[:deploy_to]}/current
 
-# Set bucket and object name
-obj = s3.buckets[ s3_bucket ].objects.with_prefix('db').collect(&:key)
-obj.each do |options, block|
-    # Read content to variable
-    file_content = block.read
-
-    # Log output (optional)
-    Chef::Log.info(file_content)
-
-    # Write content to file
-    file '/tmp/import.sql' do
-      owner 'root'
-      group 'root'
-      mode '0755'
-      content file_content
-      action :create
+        cd $HOME
+        pwd
+        tar -xvzf $FILE
+        ls -la
+        rm -rf $ROOT/wp-content/uploads
+        mv $HOME/files_develop $ROOT/wp-content/uploads
+        ls -la
+        
+        EOH
+      not_if { ::File.exists?("#{home}/files_develop") }
     end
+
+    # Import Wordpress database backup from file if it exists
+    mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' :  "-p#{node[:mysql][:server_root_password]}"} #{deploy[:database][:database]}"
+
+    Chef::Log.info("Importing Wordpress database from backup...")
+    script "import_sql" do
+      interpreter "bash"
+      user "root"
+      cwd "#{home}/"
+      code <<-EOH
+        HOME=#{home}
+        FILE=$(ls -t entre*database.sql.gz | head -1)
+        ROOT=#{deploy[:deploy_to]}/current
+        WPCLI=`wp --info`
+
+        if [ -f $FILE ]; then 
+          #{mysql_command} < $FILE;
+        fi;
+        
+        if ! type "$WPCLI" &>/dev/null; then 
+          cd $HOME
+          curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+          php wp-cli.phar --info
+          chmod +x wp-cli.phar
+          sudo mv wp-cli.phar /usr/local/bin/wp
+        else
+          cd $ROOT
+          wp search-replace 'develop-entrepreneurs-roundtable-accelerator.pantheonsite.io' 'test.eranyc.com' --allow-root
+          wp search-replace 'ec2-52-15-34-117.us-east-2.compute.amazonaws.com' 'test.eranyc.com' --allow-root
+          wp search-replace 'era.dev' 'test.eranyc.com' --allow-root
+          wp search-replace 'http://test.eranyc.com' 'https://test.eranyc.com' --allow-root
+        fi
+
+        EOH
+    end
+
 end
